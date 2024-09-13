417c7fa07c1a0cd702a3135d532d4bb47f71e24a
Your Name <jingnan.su@hotmail.com>
2024-09-13 17:55:30 +0800

    gpt 4o

--- examples/voice-assistant/minimal_assistant.py
+++ examples/voice-assistant/minimal_assistant.py
@@ -1,4 +1,5 @@
 import asyncio
+import requests
 import logging
 from dotenv import load_dotenv
 from livekit import rtc
@@ -9,17 +10,21 @@ from livekit.plugins.azure import TTS, STT
 load_dotenv()
 logger = logging.getLogger("my-worker")
 logger.setLevel(logging.INFO)
-def get_case_by_id(caseid):
-    
-    return "你是一个智能客服的教练，你正在测验客服的技能水平，你现在假设是一个奔驰车主，目前的问题是汽车无法启动了，请你打电给给客服寻求帮助，你通过这个求助电话过程中测试一下客服的服务水平，结束测验后，并给出评分。你每次回复或者提问字数尽量控制在20字以内。"
 # def get_case_by_id(caseid):
-#     url = "https://api.example.com/cases/" + caseid
-#     response = requests.get(url)
-#     if response.status_code == 200:
-#         case_data = response.json()
-#         return case_data["description"]
-#     else:
-#         return "Failed to retrieve case data"
+    
+    # return "你是一个智能客服的教练，你正在测验客服的技能水平，你现在假设是一个奔驰车主，目前的问题是汽车无法启动了，请你打电给给客服寻求帮助，你通过这个求助电话过程中测试一下客服的服务水平，结束测验后，并给出评分。你每次回复或者提问字数尽量控制在20字以内。"
+def get_case_by_id(caseid):
+    url = "https://strapi.shougan.net/api/robot-prompts/" + caseid + "?populate=robot_language,robot_type,llm,welcomeMessage"
+    response = requests.get(url)
+    if response.status_code == 200:
+            return response.json()
+            # try:
+            #     return case_data["data"]["attributes"]["ClassPrompt"]
+            # except KeyError:
+            #     return "ClassPrompt not found in case data"
+    else:
+            return "Failed to retrieve case data"
+
 async def entrypoint(ctx: JobContext):
     
 
@@ -29,18 +34,22 @@ async def entrypoint(ctx: JobContext):
     caseid= str(list(ctx.room.remote_participants.values())[0].metadata)
 
     logger.info("caseid: " + caseid)
-    text = get_case_by_id(caseid)
+    robot_case = get_case_by_id(caseid)
     initial_ctx = llm.ChatContext().append(
         role="system",
         text=(
-            text
+            robot_case["data"]["attributes"]["Prompt"]
         ),
     )
 
     assistant = VoiceAssistant(
         vad=silero.VAD.load(),
         stt=STT(languages=["zh-CN","en-US"]), # Speech-to-Text
-        llm=openai.LLM.with_ollama(base_url="http://localhost:11434/v1", model="gemma2:27b"),
+        # llm=openai.LLM.with_ollama(base_url="http://localhost:11434/v1", model="gemma2:27b"),
+        # llm=openai.LLM.with_azure(model="gpt-4o-mini", azure_endpoint="https://livekit.openai.azure.com/", azure_deployment="livekit-test", api_version="2024-02-15-preview", api_key=""),
+        # llm=openai.LLM.with_azure(model="gpt-4o", azure_endpoint="https://livekit.openai.azure.com/", azure_deployment="gpt-4o", api_version="2024-02-15-preview", api_key=""),
+        llm=openai.LLM.with_azure(model="gpt-4o", azure_endpoint="https://sunny-aoai.openai.azure.com/", azure_deployment="gpt-4o", api_version="2024-02-15-preview"),
+       
         tts=TTS(voice="zh-CN-XiaoxiaoNeural"), # Text-to-Speech
         chat_ctx=initial_ctx,
     )
@@ -62,7 +71,11 @@ async def entrypoint(ctx: JobContext):
         if msg.message:
             asyncio.create_task(answer_from_text(msg.message))
 
-    await assistant.say("你好是奔驰客户服务中心吗??", allow_interruptions=True)
+
+    welcomeMessage= robot_case["data"]["attributes"]["welcomeMessage"]
+    if welcomeMessage:
+        await assistant.say(welcomeMessage, allow_interruptions=True)
+     
 
 
 if __name__ == "__main__":
